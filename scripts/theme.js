d347120ec47aea97a31f70c76175e9d33480e337
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2022-10-04 23:26:02 -0300

    estilização concluida, falta js

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,1 +1,28 @@
-/* Desenvolva sua lógica aqui ... */
+function handleDarkMode() {
+  const dmButton = document.querySelector("#darkMode");
+  const dmIcon = document.querySelector("#dmIcon");
+  const html = document.querySelector("html");
+
+  dmButton.addEventListener("click", () => {
+    html.classList.toggle("dark-mode");
+    dmIcon.classList.toggle("text-white");
+
+    const dmPref = localStorage.getItem("darkmode");
+
+    if (!dmPref) {
+      localStorage.setItem("darkmode", true);
+    }
+
+    if (dmPref) {
+      localStorage.removeItem("darkmode");
+    }
+
+    if (dmIcon.classList.contains("text-white")) {
+      dmIcon.src = "../assets/img/sun.svg";
+    } else {
+      dmIcon.src = "../assets/img/moon.svg";
+    }
+  });
+}
+
+handleDarkMode();
